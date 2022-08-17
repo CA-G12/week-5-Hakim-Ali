905b208d4cc09567c44a2560d22b3d892921225d
alishanti98 <alialshanti90@gmail.com>
2022-08-17 16:35:23 +0300

    handle data relates #7

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,37 @@
 window.onload = () => {
   fetch('/shows')
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => objData(data))
     .catch((err) => console.log(err));
+};
+
+const objData = (data) => {
+  for (let i = 0; i < 20; i++) {
+    renderData(data[i]);
+  }
+};
+
+const renderData = (obj) => {
+  const content = document.querySelector('.content');
+  const card = document.createElement('div');
+  card.classList.add('card');
+  const img1Div = document.createElement('div');
+  img1Div.classList.add('img1');
+  const img1 = document.createElement('img');
+  img1.setAttribute('src', obj.image.medium);
+  img1Div.appendChild(img1);
+  card.appendChild(img1Div);
+
+  const titleDiv = document.createElement('div');
+  titleDiv.classList.add('title');
+  const titleText = document.createTextNode(obj.name);
+  titleDiv.appendChild(titleText);
+  card.appendChild(titleDiv);
+
+  const textDiv = document.createElement('div');
+  textDiv.classList.add('text');
+  textDiv.innerHTML = obj.summary;
+  card.appendChild(textDiv);
+
+  content.appendChild(card);
 };
